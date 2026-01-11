3d1be1de0b11e521c0a82c83784ec7c8d195ba19
nmod <33594434+neutronmoderator@users.noreply.github.com>
2026-01-11 10:09:26 +0000

    add localsend + firewall rules

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,12 @@
   networking.hostName = "fw13";
   networking.networkmanager.enable = true;
 
+  # LocalSend discovery and file transfer
+  networking.firewall = {
+    allowedTCPPorts = [ 53317 ];
+    allowedUDPPorts = [ 53317 ];
+  };
+
   # ==========================================================================
   # Localisation
   # ==========================================================================
@@ -121,6 +127,7 @@
       spotify
       rclone
       vlc
+      localsend
 
       # Games
       bolt-launcher
