d581ca1c7c7445d3077450ea10813b2eb8952359
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-07-08 10:46:33 +0100

    init with my favourite fish

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,24 @@
+set fish_greeting
+
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
+set -gx HOMEBREW_NO_INSTALL_CLEANUP 1
+set -gx HOMEBREW_NO_ENV_HINTS 1
+
+if status is-interactive
+    alias cdd 'cd ~/Developer/'
+    alias docs 'cd ~/Documents/'
+    alias oc opencode
+
+    alias gl 'git log --oneline'
+    alias gcm 'git commit -m'
+    alias gaa 'git add .'
+    alias gs 'git status'
+    alias gf 'git fetch'
+    alias gfp 'git fetch --prune'
+    alias gpl 'git pull'
+    alias gp 'git push'
+    alias gc 'git checkout'
+    alias gcb 'git checkout -b'
+    alias gcl 'git clone'
+    alias grv 'git remote -v'
+end
