10309f2dcf4db3df60947f17bafa3a4f657dca9a
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-07-08 11:35:17 +0100

    progress

--- private_dot_config/private_fish/functions/dp.fish
+++ private_dot_config/private_fish/functions/dp.fish
@@ -2,7 +2,7 @@ function dp --description 'chezmoi: re-add, commit progress, push'
     echo (set_color blue)"Refreshing Brewfile..."(set_color normal)
     brew bundle dump --global --force --quiet
     echo (set_color blue)"Finding dotfile changes..."(set_color normal)
-    chezmoi re-add --quiet
+    chezmoi re-add 2>/dev/null
     chezmoi git -- add -A
     set changed (chezmoi git -- diff --cached --name-only)
     if test (count $changed) -eq 0
